ec37e4ccff7b67c0fb26674becce18f278c2e733
Jihunkim95 <darak551@naver.com>
2023-12-26 21:53:34 +0900

    add: 이미지 파일 설정 및  버튼 클릭시 이미지 색 변경

--- ImageDocDemo/ImageDocDemo/ContentView.swift
+++ ImageDocDemo/ImageDocDemo/ContentView.swift
@@ -6,12 +6,43 @@
 //
 
 import SwiftUI
+import CoreImage
+import CoreImage.CIFilterBuiltins
 
 struct ContentView: View {
     @Binding var document: ImageDocDemoDocument
-
+    @State private var ciFilter = CIFilter.sepiaTone()
+    
+    let context = CIContext()
+    
     var body: some View {
-        TextEditor(text: $document.text)
+        VStack{
+            Image(uiImage: document.image)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .padding()
+            
+            Button{
+                filterImage()
+            }label: {
+                Text("Filter Image")
+            }
+            .padding()
+        }
+    }
+    
+    func filterImage(){
+        ciFilter.intensity = Float(1.0)
+        
+        let ciImage = CIImage(image: document.image)
+        
+        ciFilter.setValue(ciImage, forKey: kCIInputImageKey)
+        
+        guard let outputImage = ciFilter.outputImage else {return}
+        
+        if let cgImage = context.createCGImage(outputImage, from: outputImage.extent){
+            document.image = UIImage(cgImage: cgImage)
+        }
     }
 }
 
